02643feee1091f8357686c9175ce59b0ef8a1b9e
khiempham <khienph@yahoo.com>
2025-03-27 18:00:59 +0700

    fix-gcd

--- while-do_while-for/b6.1.c
+++ while-do_while-for/b6.1.c
@@ -5,15 +5,22 @@ int main() {
     printf("Nhap a: "); scanf("%d", &a);
     printf("Nhap b: "); scanf("%d", &b);
 
+
     int gcd = (a < b) ? a : b;
-    while(gcd > 0)
-    {
-       if(a % gcd == 0 && b % gcd == 0)
-         break;
-       gcd--;
+
+    if(gcd != 0) {
+      while(gcd > 0)
+      {
+         if(a % gcd == 0 && b % gcd == 0)
+           break;
+        gcd--;
+      }
+    }
+    else {
+      gcd = (a < b) ? b : a;
     }
 
-    long long lcm = (a / gcd) * b;
+    long long lcm = (a * b) / gcd;
     printf("Uoc chung lon nhat: %d\nBoi chung nho nhat: %ld\n", gcd, lcm);
     
     return 0;
